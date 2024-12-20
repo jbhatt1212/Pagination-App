e5e471ae69a352bf1ccd1ce40cf75435724f5245
jbhatt1212 <jhanavi@moontechnolabs.com>
2024-12-20 11:54:55 +0530

    Update QuoteRemoteMeditor.kt

--- app/src/main/java/com/example/paginationapp/paging/QuoteRemoteMeditor.kt
+++ app/src/main/java/com/example/paginationapp/paging/QuoteRemoteMeditor.kt
@@ -1,138 +1,51 @@
 package com.example.paginationapp.paging
 
+import android.util.Log
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
 import androidx.room.withTransaction
-import com.example.paginationapp.Retrofit.QuoteApi
-import com.example.paginationapp.db.QuotesDatabase
-import com.example.paginationapp.model.Result
+import com.example.paginationapp.API.ApiService
+import com.example.paginationapp.db.QuoteDatabase
+import com.example.paginationapp.model.Quote
 import com.example.paginationapp.model.QuoteRemoteKeys
 
 @ExperimentalPagingApi
 class QuoteRemoteMediator(
-    private val quoteAPI: QuoteApi,
-    private val quoteDatabase: QuotesDatabase
-) : RemoteMediator<Int, Result>() {
+    private val apiService: ApiService,
+    private val quoteDatabase: QuoteDatabase
+) : RemoteMediator<Int, Quote>() {
 
-    val quoteDao = quoteDatabase.quoteDao()
-    val quoteRemoteKeysDao = quoteDatabase.remoteKeysDao()
-
-    override suspend fun load(loadType: LoadType, state: PagingState<Int, Result>): MediatorResult {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, Quote>
+    ): MediatorResult {
         return try {
-
-            val currentPage = when (loadType) {
-                LoadType.REFRESH -> {
-                    val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
-                    remoteKeys?.nextPage?.minus(1) ?: 1
-                }
-                LoadType.PREPEND -> {
-                    val remoteKeys = getRemoteKeyForFirstItem(state)
-                    val prevPage = remoteKeys?.prevPage
-                        ?: return MediatorResult.Success(
-                            endOfPaginationReached = remoteKeys != null
-                        )
-                    prevPage
-                }
+            val page = when (loadType) {
+                LoadType.REFRESH -> 0
+                LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
                 LoadType.APPEND -> {
-                    val remoteKeys = getRemoteKeyForLastItem(state)
-                    val nextPage = remoteKeys?.nextPage
-                        ?: return MediatorResult.Success(
-                            endOfPaginationReached = remoteKeys != null
-                        )
-                    nextPage
+                    val lastItem = state.lastItemOrNull()
+                        ?: return MediatorResult.Success(endOfPaginationReached = true)
+                    state.pages.size
                 }
             }
 
-            val response = quoteAPI.getQuotes(currentPage)
-            val endOfPaginationReached = response.totalPages == currentPage
-
-            val prevPage = if(currentPage == 1) null else currentPage -1
-            val nextPage = if(endOfPaginationReached) null else currentPage + 1
+            val response = apiService.getQuotes(skip = page * state.config.pageSize, limit = state.config.pageSize)
 
             quoteDatabase.withTransaction {
 
                 if (loadType == LoadType.REFRESH) {
-                    quoteDao.deleteQuotes()
-                    quoteRemoteKeysDao.deleteAllRemoteKeys()
+                    quoteDatabase.quoteDao().deleteQuotes()
                 }
-
-                quoteDao.addQuotes(response.results)
-                val keys = response.results.map { quote ->
-                    QuoteRemoteKeys(
-                        id = quote._id,
-                        prevPage = prevPage,
-                        nextPage = nextPage
-                    )
-                }
-                quoteRemoteKeysDao.addAllRemoteKeys(keys)
+                quoteDatabase.quoteDao().addQuotes(response.quotes)
             }
-            MediatorResult.Success(endOfPaginationReached)
-        }
-        catch (e: Exception){
+
+            MediatorResult.Success(endOfPaginationReached = response.quotes.isEmpty())
+        } catch (e: Exception) {
             MediatorResult.Error(e)
         }
     }
 
-    private suspend fun getRemoteKeyClosestToCurrentPosition(
-        state: PagingState<Int, Result>
-    ): QuoteRemoteKeys? {
-        return state.anchorPosition?.let { position ->
-            state.closestItemToPosition(position)?._id?.let { id ->
-                quoteRemoteKeysDao.getRemoteKeys(id = id)
-            }
-        }
-    }
-
-    private suspend fun getRemoteKeyForFirstItem(
-        state: PagingState<Int, Result>
-    ): QuoteRemoteKeys? {
-        return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()
-            ?.let { quote ->
-                quoteRemoteKeysDao.getRemoteKeys(id = quote._id)
-            }
-    }
-
-    private suspend fun getRemoteKeyForLastItem(
-        state: PagingState<Int, Result>
-    ): QuoteRemoteKeys? {
-        return state.pages.lastOrNull { it.data.isNotEmpty() }?.data?.lastOrNull()
-            ?.let { quote ->
-                quoteRemoteKeysDao.getRemoteKeys(id = quote._id)
-            }
-    }
 }
-
-//    override suspend fun load(
-//        loadType: LoadType,
-//        state: PagingState<Int, Quote>
-//    ): MediatorResult {
-//        return try {
-//            val page = when (loadType) {
-//                LoadType.REFRESH -> 0
-//                LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
-//                LoadType.APPEND -> {
-//                    val lastItem = state.lastItemOrNull()
-//                        ?: return MediatorResult.Success(endOfPaginationReached = true)
-//                    state.pages.size
-//                }
-//            }
-//
-//            val response = apiService.getQuotes(skip = page * state.config.pageSize, limit = state.config.pageSize)
-//
-//            quoteDatabase.withTransaction {
-//
-//                if (loadType == LoadType.REFRESH) {
-//                    quoteDatabase.quoteDao().deleteQuotes()
-//                }
-//                quoteDatabase.quoteDao().addQuotes(response.quotes)
-//            }
-//
-//            MediatorResult.Success(endOfPaginationReached = response.quotes.isEmpty())
-//        } catch (e: Exception) {
-//            MediatorResult.Error(e)
-//        }
-//    }
-
-
