46c30650a2cbc7ef7d3f03910a113ce61fa882cd
jbhatt1212 <jhanavi@moontechnolabs.com>
2024-12-20 11:55:34 +0530

    Update QuotePagingSource.kt

--- app/src/main/java/com/example/paginationapp/paging/QuotePagingSource.kt
+++ app/src/main/java/com/example/paginationapp/paging/QuotePagingSource.kt
@@ -2,35 +2,43 @@ package com.example.paginationapp.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import com.example.paginationapp.model.Result
+import com.example.paginationapp.API.ApiService
+import com.example.paginationapp.model.Quote
 
-import com.example.paginationapp.Retrofit.QuoteApi
 
-
-class QuotePagingSource(private val quoteApi :QuoteApi) :PagingSource<Int, Result>() {
+class QuotePagingSource(private val quoteApi :ApiService) :PagingSource<Int, Quote>() {
     companion object {
-        private const val STARTING_PAGE_INDEX = 0
+        private const val STARTING_PAGE_INDEX = 0 // Start pagination from 0
+        private const val LIMIT = 30 // Number of items per page
     }
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Result> {
-        return try {
+        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Quote> {
             val position = params.key ?: STARTING_PAGE_INDEX
-            val response = quoteApi.getQuotes(position)
 
-            return LoadResult.Page(
-                data = response.results,
-                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
-                nextKey = if (position == response.totalPages) null else position + 1
-            )
+            return try {
+                val response = quoteApi.getQuotes(
+                    skip = position * LIMIT,
+                    limit = LIMIT
+                )
 
-        } catch (e: Exception) {
-            LoadResult.Error(e)
-        }
+
+                val totalPages = (response.total + LIMIT - 1) / LIMIT
+
+                LoadResult.Page(
+                    data = response.quotes,
+                    prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
+                    nextKey = if (position >= totalPages - 1) null else position + 1
+                )
+       } catch (e:Exception){
+           LoadResult.Error(e)
+       }
     }
 
-    override fun getRefreshKey(state: PagingState<Int, Result>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, Quote>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
             anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
         }
     }
+
+
 }
